82dcdbdd677da7f690a7f0abe1f5b68eee8ba75a
gothIhop123 <nathan.bazar@marsden-hs.nsw.edu.au>
2025-12-07 05:06:36 +1100

    Refactor dice rolling logic and add d20 functionality

--- PWA app/js/app.js
+++ PWA app/js/app.js
@@ -3,14 +3,18 @@ const roll = document.getElementById('roll');
 const label = document.getElementById('label'); 
 const checkNum = document.getElementById('checkNum')
 const dice = document.getElementById('dice')
+const body = document.getElementById('body')
+const d20 = document.getElementById('d20')
+const d20roll = document.getElementById('d20roll')
+
 dice.src = `images/dice1.png`;
-let randomNum;
+let roll_times = 2;                           //times you can roll before being unable to
+let pausetime = 1800;                           //constant time it takes for delays
+let randomNum = Math.floor(Math.random() * (6 /*max*/ - 6 /*min*/ + 1)) + 6 /*min*/; //randomise a number from 1 to 6
+let quarterchance = Math.floor(Math.random() * 4) + 1; //randomise a number from 1 to 4
 
-function dice_outcomes(){ //dice roll and outcomes
-    randomNum = Math.floor(Math.random() * (6 /*max*/ - 6 /*min*/ + 1)) + 6 /*min*/; //randomise a number 
-
+function dice_animation(){ //dice roll and outcomes
     let frames = 20;
-    let frameTime = 80;
 
     let animation = setInterval(() => {
         let temp = Math.floor(Math.random() * 6) + 1;
@@ -19,40 +23,61 @@ function dice_outcomes(){ //dice roll and outcomes
             if (frames <= 0) {
                 clearInterval(animation);
                 dice.src = `images/dice${randomNum}.png`;
-                label.textContent = randomNum; //display num
+                label.textContent = randomNum; //display final num
+                outcomes(randomNum);
             };
-    }, frameTime);
-    
-    switch(randomNum) { //checks number
+    }, 80);
+};
+
+function outcomes() {s
+    setTimeout(() => {
+        switch(randomNum) { //checks number
+        case 3:                                         //inverses the color of the page
+            body.style.background = 'black';
+            h1.style.color = 'white';
+            label.style.color = 'white';
+            case3or4()
+            break;
+        case 4:
+            checkNum.textContent = 'Num is 3 or 4';     //check if Num is 1 or 4
+            checkNum.style.display = 'show';
+            break;
+        case 2:
+
+            break;
+        case 5:
+            checkNum.textContent = 'Num is 2 or 5';     //check if Num is 2 or 5
+        case 1:
+
+            break;
+        case 6:                     //if num is 6 there is a chance that case6 occurs
+            case6();
+            break;
+        };
+    }, 100);
+}
+
+function case6() {                                   //reveals a d20 dice roll
+    d20roll.removeAttribute("hidden");
+    d20.removeAttribute("hidden");
+};
+
+function case3or4() {
+    switch(quarterchance) {
+    case 1:
+    case 2:
+        body.style.background = 'white';
+        h1.style.color = 'black';
+        label.style.color = 'black';
+        break;
     case 3:
-    case 4:
-        checkNum.textContent = 'Num is 3 or 4';     //check if Num is 1 or 4
+        
         break;
-    case 2:
-    case 5:
-        checkNum.textContent = 'Num is 2 or 5';     //check if Num is 2 or 5
-        break;
-    case 1:
-        break;
-    case 6:
-        if (randomNum === 6) {                      //if num is 6 there is a chance that case6 occurs
-            setTimeout(() => {
-                case6();
-            }, 500);
-        }
-        break;
-    };
-};
-
-function case6(){                                   //1 in 67 chance to alert 67
-    if (Math.random() < 1 / 67) {
-        window.alert(67)
     }
 };
 
-function case2or5(){
-    chance = Math.floor(Math.random() * 4) + 1;
-    switch(chance) {
+function case2or5() {
+    switch(quarterchance) {
     case 1:
         
         break;
@@ -62,38 +87,64 @@ function case2or5(){
     }
 };
 
-function case3or4(){
-    chance = Math.floor(Math.random() * 4) + 1;
-    switch(chance) {
-    case 1:
-        
-        break;
-    case 2:
-        
-        break;
-    }
-};
-
-function case1(){
+function case1() {
     if (Math.random() < 1/25) {                       //1 in 25 chance to open new blank tabs
         let i = 2;
         while (i <= 0) {
-            window.open('about:blank', 'blank').focus();
+            window.open('https://pornhub.com', 'about:idk').focus();
             i--;
         }
     }
+};
+
+function d20animation() {
+    let d20num = Math.floor(Math.random() * (7 /*max*/ - 7 /*min*/ + 1)) + 7 /*min*/; //randomise a number 
+    let frames = 20;
+
+    let animation = setInterval(() => {
+        let temp = Math.floor(Math.random() * 20) + 1;      //animation for d20 roll
+        d20.textContent = temp;
+        frames--;
+        if (frames <= 0) {
+            clearInterval(animation);
+            d20.textContent = d20num; //display final num
+            setTimeout(() => {
+                switch(d20num) {
+                case 1:
+                    break;
+                case 7:
+                    window.alert('67')
+                };
+            }, 100);
+        };
+    }, 80);
 };
 
 roll.onclick = function() {
     roll.disabled = true;
 
     if (Math.random() < 1/3) {
-        dice_outcomes();
-            setTimeout(() => {
+        dice_animation();
+//        roll_times--;
+      
+        setTimeout(() => {
+            if (roll_times <= 0) {
+                roll.disabled = true;
+            } else {
                 roll.disabled = false;
-            }, 1800);
+            }
+        }, pausetime);
+
     } else {
         label.textContent = 'Try again?';
         roll.disabled = false;
-    }
+    };
+};
+
+d20roll.onclick = function() {
+    d20roll.disabled = true;
+    d20animation();
+    setTimeout(() => {
+        d20roll.disabled = false;
+    }, pausetime);
 };
